73c287c8e2701789ef6a0ca339187bfe6781f1e5
hyolim lim <hyolimlim.dev@gmail.com>
2023-03-08 00:21:24 +0900

    feat: add notes total count

--- src/queries/notesQuery.ts
+++ src/queries/notesQuery.ts
@@ -1,6 +1,7 @@
 export const notesQuery = {
   getNotes:
     "SELECT * FROM notes WHERE user_id = ? ORDER BY id DESC LIMIT ? OFFSET ?",
+  countTotalNotes: "SELECT COUNT(*) AS count FROM notes WHERE user_id = ?",
   addNotes:
     "INSERT INTO notes (user_id, title, content, created_at) VALUES (?, ?, ?, now())",
   editNotes:
