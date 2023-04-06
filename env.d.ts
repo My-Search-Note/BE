a28ecce34f3b918003d63ea7ab754a7f92ba7810
hyolim lim <hyolimlim.dev@gmail.com>
2023-04-06 15:56:32 +0900

    feat: convert port number

--- env.d.ts
+++ env.d.ts
@@ -5,5 +5,6 @@ declare namespace NodeJS {
     DB_USER: string;
     DB_PASSWORD: string;
     DB_DATABASE: string;
+    DB_PORT: number;
   }
 }
--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10),
 });
 
 export default pool;
